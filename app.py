cac70f8a683ccd8a9716cfe8734c374d311f5291
Prabin <42871240+prabincankod@users.noreply.github.com>
2023-08-08 14:11:33 +0545

    :sparkles: init commit for somthing nice

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/get-hook',methods=['POST','GET'])
+def get_hook():
+    if request.method == 'POST':
+        print(request.json)
+    return 'Hello, World!'
