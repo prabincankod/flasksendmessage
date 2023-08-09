8cc5f4a6cae11bd8452edb1854aeb7e526774e36
Prabin <42871240+prabincankod@users.noreply.github.com>
2023-08-09 22:05:02 +0545

    minor bug again

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app_password = os.getenv('app_password')
 
 # Recipient email
 def genEmail(commitMessage,commitLink):
-    generatedEmail = f"""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    generatedEmail = f'''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
     <html>
 
     <head>
@@ -395,8 +395,7 @@ def genEmail(commitMessage,commitLink):
         </table>
     </body>
 
-    </html>
-"""
+    </html>'''
     return generatedEmail
 
 
